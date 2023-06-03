fb9757a70d1c30641168fddf11a6588e50466f1f
WahidAhmad-98129 <wahidHadayat.personal.info@gmail.com>
2023-06-03 08:57:52 +0430

    writing the title of the appbar

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FirstApp());
+
+class FirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        appBar: AppBar(
+          title:Center(child: Text('I AM RICH'),),
+        ),
+      ),
+    );
+  }
+}
+
+
